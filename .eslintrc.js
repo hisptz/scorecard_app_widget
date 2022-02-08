83ff1acdac148e4dec8444ae6c6784405f3571cf
Danford Kija <kijadanford@gmail.com>
2022-02-08 12:54:59 +0300

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+/** @format */
+
+module.exports = {
+  extends: ['react-app'],
+  rules: {
+      'no-console': 'off',
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() :React.ReactElement {
+  return (
+    <div className="App">
+          ScoreCard Widget
+    </div>
+  );
+}
+
+export default App;
