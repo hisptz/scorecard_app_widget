8cbc8f0a47b6eed3463b7682de0d0270b80bcd66
Danford Kija <kijadanford@gmail.com>
2022-02-10 18:08:42 +0300

    feat(types):add types for modules with non types

---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+declare module "@dhis2/ui";
+declare module "@dhis2/d2-i18n";
+declare module "*.module.css";
+declare module "react-web-vitals";
+declare module '@iapps/function-analytics';
+declare module 'lodash';
+declare module 'react-color';
+declare module 'async/mapLimit';
+declare module '@dhis2/analytics/build/cjs/components/TransferOption';
+/* eslint-enable */
