9a7ae9c896926e3a4bb2326ecdb06cf4a8ab307f
Danford Kija <kijadanford@gmail.com>
2022-02-14 15:35:13 +0300

    feat(app-module):implement app module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import { App } from './App'
 import ReactDOM from 'react-dom'
-import { App } from './components/App'
+import React from 'react'
+import { Provider } from '@dhis2/app-runtime'
 
 const rootElement = document.getElementById('root')
 
-ReactDOM.render(<App />, rootElement)
+const productionRender = async () => {
+    try {
+        const manifest = await (await fetch('./manifest.webapp')).json()
+        render(manifest.activities.dhis.href)
+    } catch (error) {
+        console.error('Could not read manifest:', error)
+        ReactDOM.render(<code>No manifest found</code>, rootElement)
+    }
+}
+
+const render = baseUrl =>
+    ReactDOM.render(
+        ReactDOM.render(
+            <Provider
+                config={{
+                    baseUrl: baseUrl,
+                    apiVersion: 34,
+                }}
+            >
+                <App />
+            </Provider>,
+            rootElement
+        ),
+        rootElement
+    )
+
+productionRender()
