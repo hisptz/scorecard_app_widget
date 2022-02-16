9bda8b96e5c9b7c6d2f0e4a59a2f4e0dba560eba
Danford Kija <kijadanford@gmail.com>
2022-02-16 10:30:34 +0300

    refactor:raname datastore namespace pointer for widgets

--- src/core/constants/config.js
+++ src/core/constants/config.js
@@ -2,7 +2,7 @@ export const APP_NAME = "Scorecard";
 export const DATASTORE_NAMESPACE = "hisptz-scorecard";
 export const DATASTORE_ENDPOINT = "dataStore/hisptz-scorecard";
 export const DATASTORE_SCORECARD_SUMMARY_KEY = "scorecard-summary";
-export const DATASTORE_SCORECARD_WIDGET ="hisptz-scorecardwidget";
+export const DATASTORE_SCORECARD_WIDGET = "dataStore/hisptz-scorecardwidget";
 export const DATASTORE_SCORECARD_SUMMARY_INCLUDE_KEYS = [
   {
     key: "title",
