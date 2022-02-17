8d08feb1d197e907a48c6f4408922758e04dd044
Gift Nnko <nnkogift@hotmail.com>
2022-02-17 14:29:01 +0300

    feat(index.js): Implementation
    - Implemented dynamic fetching of apiVersion on app start

--- src/index.js
+++ src/index.js
@@ -1,46 +1,48 @@
-import { App } from './App'
+import {App} from './App'
 import ReactDOM from 'react-dom'
 import React from 'react'
-import { Provider } from '@dhis2/app-runtime'
+import {Provider} from '@dhis2/app-runtime'
 
 const rootElement = document.getElementById('root')
 
-if(process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'production') {
     const productionRender = async () => {
         try {
             const manifest = await (await fetch('./manifest.webapp')).json()
-            render(manifest.activities.dhis.href)
+            const [, apiVersion] = (await fetch(`${manifest.activities.dhis.href}/api/system/info.json`)
+                .then(res => res.json())
+                .then((systemInfo) => systemInfo.version)).split('.') ?? []
+            render(manifest.activities.dhis.href, apiVersion)
         } catch (error) {
             console.error('Could not read manifest:', error)
             ReactDOM.render(<code>No manifest found</code>, rootElement)
         }
     }
 
-    const render = baseUrl =>
-    ReactDOM.render(
-        <Provider
+    const render = (baseUrl, apiVersion) =>
+        ReactDOM.render(
+            <Provider
                 config={{
-                    baseUrl: baseUrl,
-                    apiVersion: 34,
+                    baseUrl,
+                    apiVersion,
                 }}
             >
-                <App />
+                <App/>
             </Provider>,
-        rootElement
-    )
+            rootElement
+        )
 
     productionRender()
-}
-else{
+} else {
     ReactDOM.render(
         <Provider
-        config={{
-            baseUrl: "http://localhost:8081",
-            apiVersion: 34,
-        }}
-    >
-       <App />
-    </Provider>,
+            config={{
+                baseUrl: "http://localhost:8083",
+                apiVersion: 37,
+            }}
+        >
+            <App/>
+        </Provider>,
         rootElement
     )
 }
